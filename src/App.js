c3b760daa2e804ecfade2c4efab3da65e996615a
Saqib <zaman342003@gmail.com>
2022-05-24 23:37:45 +0500

    This is office task

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/Navbar';
+import Profile from './components/Profile';
+import Hope from './components/hope';
+ 
+function App() {
+  return (
+    <div className="App">
+       {/* <NavBar /> */}
+       <Profile />
+       {/* <Hope /> */}
+    </div>
+  );
+}
+
+export default App;
